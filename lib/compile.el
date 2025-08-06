51d14101527eb245678cd7b31ffe3b5e1d9bef47
Jonas Bernoulli <jonas@bernoul.li>
2025-08-07 00:45:49 +0200

    compile: No longer inhibit check-declare target

--- lib/compile.el
+++ lib/compile.el
@@ -9,3 +9,21 @@
        (if (eq (car binding) '_)
            (cons (make-symbol "s") (cdr binding))
          binding)))))
+
+(when (< emacs-major-version 30)
+  (require 'subr-x) ;for when-let*
+  (advice-add
+   'check-declare-directory :override
+   (lambda (root)
+     (interactive "DDirectory to check: ")
+     (setq root (directory-file-name (file-relative-name root)))
+     (or (file-directory-p root)
+         (error "Directory `%s' not found" root))
+     (when-let* ((files (directory-files-recursively root "\\.el\\'"))
+                 (files (mapcan (lambda (file)
+                                  ;; Filter out lock files.
+                                  (and (not (string-prefix-p
+                                             ".#" (file-name-nondirectory file)))
+                                       (list file)))
+                                files)))
+       (apply #'check-declare-files files)))))
