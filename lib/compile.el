881c4d7459569369935b9a5db011f93c8ee8ffed
Jonas Bernoulli <jonas@bernoul.li>
2025-08-07 00:45:48 +0200

    compile: Backport fix from Emacs 30 for bogus COND-let warning
    The macro expansions did not leave a variable `_' unused; this
    triggered an irritating compiler warning.
    
    Backport fix for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=69108,
    from Emacs commit e680827e814e155cf79175d87ff7c6ee3a08b69a.

--- lib/compile.el
+++ lib/compile.el
@@ -1,2 +1,11 @@
 (put 'if-let   'byte-obsolete-info nil)
 (put 'when-let 'byte-obsolete-info nil)
+
+(when (< emacs-major-version 30)
+  (advice-add
+   'internal--build-binding :around
+   (lambda (fn binding prev-var)
+     (let ((binding (funcall fn binding prev-var)))
+       (if (eq (car binding) '_)
+           (cons (make-symbol "s") (cdr binding))
+         binding)))))
